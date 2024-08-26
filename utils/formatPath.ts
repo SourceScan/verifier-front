1f65a62edbd39993c81f6e486abfce5fa81278d7
Andrey Gruzdev <agruzdev1@gmail.com>
2024-08-26 13:45:59 +0200

    fix: rever formatSourceCodePath

--- utils/formatPath.ts
+++ utils/formatPath.ts
@@ -3,7 +3,7 @@ const formatSourceCodePath = (path: String, lang: String) => {
 
   segments.pop()
   if (lang === 'rust') {
-    segments.push('src/lib.rs')
+    segments.push('src')
   }
 
   return segments.join('/')
